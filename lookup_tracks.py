80e9ca86c3cffdaa9cdf0215ad2dbcc1b2294d53
Alastair Porter <alastair@porter.net.nz>
2016-04-29 13:00:59 +0200

    look up songs one at a time, to get redirect data
    also add logging/estimated time remaining

--- lookup_tracks.py
+++ lookup_tracks.py
@@ -3,7 +3,17 @@ import db
 import db.data
 import logging
 import util
-#logging.basicConfig(level=logging.DEBUG)
+import time
+import datetime
+
+import logging
+logfmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+lookuplog = logging.Logger('lookup')
+ch = logging.StreamHandler()
+ch.setFormatter(logfmt)
+lookuplog.setLevel(logging.INFO)
+lookuplog.addHandler(ch)
 
 import concurrent.futures
 import echonest
@@ -14,47 +24,57 @@ def query(songid, token):
 
 
 def lookup():
+    """ returns (done, remaining)"""
     songs = db.data.get_pending_songs()
+    songcount = db.data.get_count_pending_songs()
+
     if not songs:
-        return False
+        return (0, 0)
+
     # We can use a with statement to ensure threads are cleaned up promptly
-    with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         # Start the load operations and mark each future with its URL
         i = 0
         future_to_song = {}
-        for ss in util.chunks(songs, 10):
-            future_to_song[executor.submit(query, ss, i)] = ss
+        for song in songs:
+            future_to_song[executor.submit(query, song, i)] = song
             i = 1 - i
 
         for future in concurrent.futures.as_completed(future_to_song):
-            ss = future_to_song[future]
+            songid = future_to_song[future]
             # For each set of songs, get them from the response
             # for songs not in the response, add an empty response
             try:
                 data = future.result()
             except Exception as exc:
-                print('%r generated an exception: %s' % (url, exc))
+                print('%r generated an exception: %s' % (songid, exc))
             else:
-                gotsongs = set()
-                waitings = set(ss)
-                results = data["response"].get("songs", [])
-                for s in results:
-                    songid = s["id"]
-                    gotsongs.add(songid)
-                    db.data.add_response_if_not_exists(echonest.SONG_PROFILE, songid, s)
-                nosongs = waitings-gotsongs
-                for s in list(nosongs):
-                    db.data.add_response_if_not_exists(echonest.SONG_PROFILE, s, {})
-    return True
+                db.data.add_response_if_not_exists(echonest.SONG_PROFILE, songid, data)
+    return (len(songs), songcount-len(songs))
+
+def stats(done, total, starttime):
+    nowtime = time.time()
+    position = done*1.0 / total
+    duration = round(nowtime - starttime)
+    durdelta = datetime.timedelta(seconds=duration)
+    remaining = round((duration / position) - duration)
+    remdelta = datetime.timedelta(seconds=remaining)
+
+    return str(durdelta), str(remdelta)
+
 
 def main():
     db.init_db_engine(config.SQLALCHEMY_DATABASE_URI)
-    res = lookup()
-    i = 0
-    while res:
-        print("{}".format(i))
-        res = lookup()
-        i += 1
+    total = db.data.get_count_pending_songs()
+    done = 0
+    starttime = time.time()
+    thisdone, rem = lookup()
+    done += thisdone
+    while rem > 0:
+        thisdone, rem = lookup()
+        done += thisdone
+        durdelta, remdelta = stats(done, total, starttime)
+        lookuplog.info("Done %s/%s in %s; %s remaining", done, total, str(durdelta), str(remdelta))
 
 if __name__ == "__main__":
     main()
